12d59ea6d8a44533936ec0b4fbf3bd80e314a91b
Richard Rodas <richard_rodas01@hotmail.com>
2025-06-11 19:25:54 -0500

    feat(mi-primera-creatina): añadir navbar y footer al componente
    - Se agregó el componente `app-navbar` con el tipo `NavbarTypeEnum.LANDING` para mejorar la navegación en la página de "Mi Primera Creatina".
    - Se incluyó el componente `app-footer` al final de la página, proporcionando una mejor estructura y presentación.
    - Se actualizaron las importaciones en el componente para incluir `HeaderComponent`, `FooterComponent` y `NavbarComponent`.
    
    Estos cambios contribuyen a una experiencia de usuario más completa y organizada en la sección de "Mi Primera Creatina".

--- src/app/ecommerce/landing/pages/mi-primera-creatina/mi-primera-creatina.component.ts
+++ src/app/ecommerce/landing/pages/mi-primera-creatina/mi-primera-creatina.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../../../../environments/env';
+import { HeaderComponent } from '../../../signup/components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { NavbarComponent, NavbarTypeEnum } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-mi-primera-creatina',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent, NavbarComponent],
   templateUrl: './mi-primera-creatina.component.html',
   styleUrl: './mi-primera-creatina.component.css'
 })
@@ -15,7 +18,7 @@ export class MiPrimeraCreatinaComponent implements OnInit {
   
   lote = signal<string>('');
   ENV = environment;
-
+  NavbarTypeEnum = NavbarTypeEnum;
   ngOnInit(): void {
     // Obtener el parámetro de query 'lote'
     this.route.queryParams.subscribe(params => {
